79164edd812dc7d174caec32905ed47c170ad19e
Darius Jankauskas <d5926j@gmail.com>
2023-03-25 02:30:26 -0400

    uno mas

--- docs/some_assembly_required-824b778ca0dcf42e.js
+++ docs/some_assembly_required-824b778ca0dcf42e.js
@@ -551,7 +551,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('some_assembly_required-36bf52a92dbec753_bg.wasm', import.meta.url);
+        input = new URL('some_assembly_required-824b778ca0dcf42e_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
